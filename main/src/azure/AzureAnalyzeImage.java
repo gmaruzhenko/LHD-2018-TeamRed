4cc86eda22bfae97e0be61c6e90f5fb53bcfbb05
audrey <audrey_lu@outlook.com>
2018-12-01 14:36:20 -0800

    2. merge conflict

---
+++ main/src/azure/AzureAnalyzeImage.java
@@ -0,0 +1,20 @@
+// test commit
+
+import com.google.gson.*;
+
+// input is JSON string, output is boolean
+// if person, then do output false
+
+// if false then do not update
+
+
+// update: add photo to list
+
+public class AzureAnalizeImage {
+
+    public boolean fnName() {
+        JSON.stringify();
+    }
+
+
+}
